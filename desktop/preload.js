143a1242030ebbf9d55521303b29c240bcad6ca6
XxHuberrr <281847693+XxHuberrr@users.noreply.github.com>
2026-06-28 22:46:53 +0800

    fix youtube music playback

--- desktop/preload.js
+++ desktop/preload.js
@@ -12,6 +12,8 @@ contextBridge.exposeInMainWorld('desktopWindow', {
   clearNeteaseMusicLogin: () => ipcRenderer.invoke('netease-music-clear-login'),
   openQQMusicLogin: () => ipcRenderer.invoke('qq-music-open-login'),
   clearQQMusicLogin: () => ipcRenderer.invoke('qq-music-clear-login'),
+  openYoutubeMusicLogin: () => ipcRenderer.invoke('youtube-music-open-login'),
+  clearYoutubeMusicLogin: () => ipcRenderer.invoke('youtube-music-clear-login'),
   openUpdateInstaller: (filePath) => ipcRenderer.invoke('mineradio-open-update-installer', filePath),
   restartApp: () => ipcRenderer.invoke('mineradio-restart-app'),
   configureGlobalHotkeys: (bindings) => ipcRenderer.invoke('mineradio-hotkeys-configure-global', bindings || []),
