7b5e75d81db8a52fb1c6b39f40160a3dc19d75ed
17675 <you@example.com>
2026-08-12 15:56:48 +0800

    Fix: remove nested src directory

--- src/src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  selectFolder: () => ipcRenderer.invoke('select-folder'),
-  scanFolder: (dir) => ipcRenderer.invoke('scan-folder', dir),
-  getState: () => ipcRenderer.invoke('get-state'),
-  saveState: (s) => ipcRenderer.invoke('save-state', s),
-  readFile: (path) => ipcRenderer.invoke('read-file', path)
-});
